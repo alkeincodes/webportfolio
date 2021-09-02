8da8e485f35ab75b460ee30b4fdc5552f38fe596
Alkein Villajos <alkein.developments@gmail.com>
2021-09-03 05:07:53 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       // { 'http-equiv': "Content-Security-Policy", content: 'upgrade-insecure-requests' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: 'https://alkeinvillajos.github.io/images/logo-icon.png' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
     ]
   },
